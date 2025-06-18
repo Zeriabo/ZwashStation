a2a3d4b460db0038a5371255ca56546a42399eef
Zeriab Chiah <zeriab@Zeriabs-MBP.lan>
2025-06-18 14:37:06 +0300

    update Zwash Station
    Fixing routes

--- src/main/java/com/zwash/station/controller/StationController.java
+++ src/main/java/com/zwash/station/controller/StationController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.zwash.common.dto.StationDTO;
 import com.zwash.common.pojos.CarWashingProgram;
@@ -45,7 +43,7 @@ public class StationController {
 
 	@ApiOperation("Get all station washes")
 	@GetMapping("/washes")
-	public ResponseEntity<List<CarWashingProgram>> getStationWashes(@RequestParam Long id) throws StationNotExistsException {
+	public ResponseEntity<List<CarWashingProgram>> getStationWashes(@RequestParam("id") Long id) throws StationNotExistsException {
 		//getStationWashed
 		List<CarWashingProgram> list = stationService.getStationWashed(id);
 	
